071d843532ece510ca6bfe86171ad4ca3a850804
Yazid <yazid780@outlook.fr>
2024-09-03 18:55:58 +0200

    js end ,deskop end

--- src/index.js
+++ src/index.js
@@ -4,10 +4,87 @@ async function fetchData() {
   return data;
 }
 
+const daily = document.getElementById("daily");
+const weekly = document.getElementById("weekly");
+const monthly = document.getElementById("monthly");
+
 async function processData() {
   const data = await fetchData();
-  data.forEach((element) => {
-    console.log("d", element.timeframes.daily.current);
+
+  const theme = document.querySelectorAll(".theme");
+  const trackingTime = document.querySelectorAll(".trackingTime");
+
+  theme.forEach((theme, index) => {
+    const title = document.createElement("h3");
+
+    title.textContent = data[index].title;
+    theme.appendChild(title);
+  });
+
+  function clearTrackingTime() {
+    trackingTime.forEach((tracking) => {
+      const typeTrackings = tracking.querySelectorAll(".typeTracking");
+      typeTrackings.forEach((typeTracking) => {
+        tracking.removeChild(typeTracking);
+      });
+    });
+  }
+  function Daily() {
+    trackingTime.forEach((trackingTime, index) => {
+      const container = document.createElement("div");
+      const dailyCurrent = document.createElement("h1");
+      const dailyPrevious = document.createElement("p");
+      container.classList.add("typeTracking");
+      dailyCurrent.textContent = data[index].timeframes.daily.current + "hrs";
+
+      dailyPrevious.textContent = "Yesterday" + " " + "-" + data[index].timeframes.daily.previous + "hrs";
+      container.appendChild(dailyCurrent);
+      container.appendChild(dailyPrevious);
+      trackingTime.appendChild(container);
+    });
+  }
+
+  function Weekly() {
+    trackingTime.forEach((trackingTime, index) => {
+      const container = document.createElement("div");
+      const dailyCurrent = document.createElement("h1");
+      const dailyPrevious = document.createElement("p");
+      container.classList.add("typeTracking");
+
+      dailyCurrent.textContent = data[index].timeframes.weekly.current + "hrs";
+      dailyPrevious.textContent = "Last Week" + " " + "-" + data[index].timeframes.weekly.previous + "hrs";
+      container.appendChild(dailyCurrent);
+      container.appendChild(dailyPrevious);
+      trackingTime.appendChild(container);
+    });
+  }
+  function Monthly() {
+    trackingTime.forEach((trackingTime, index) => {
+      const container = document.createElement("div");
+      const dailyCurrent = document.createElement("h1");
+      const dailyPrevious = document.createElement("p");
+      container.classList.add("typeTracking");
+
+      dailyCurrent.textContent = data[index].timeframes.monthly.current + "hrs";
+      dailyPrevious.textContent = "Last Month" + " " + "-" + data[index].timeframes.monthly.previous + "hrs";
+      container.appendChild(dailyCurrent);
+      container.appendChild(dailyPrevious);
+      trackingTime.appendChild(container);
+    });
+  }
+
+  Weekly();
+  daily.addEventListener("click", () => {
+    clearTrackingTime();
+    Daily();
+  });
+  weekly.addEventListener("click", () => {
+    clearTrackingTime();
+    Weekly();
+  });
+  monthly.addEventListener("click", () => {
+    clearTrackingTime();
+    Monthly();
   });
 }
 
